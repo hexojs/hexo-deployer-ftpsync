34efc5dc495ef5d4ae33edc6693ca91d4557144f
curbengh <43627182+curbengh@users.noreply.github.com>
2019-08-03 14:09:10 +0930

    test: add unit test, eslint and travis

---
+++ test/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const should = require('chai').should(); // eslint-disable-line
+
+describe('hexo-deployer-ftpsync', () => {
+  // Tests.
+  it('should deploy through ftp.', () => {
+    return true;
+  });
+});
